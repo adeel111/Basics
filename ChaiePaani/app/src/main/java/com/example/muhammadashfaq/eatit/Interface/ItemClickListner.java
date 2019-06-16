236932c6f2ba57a4179a009abde263808949ad8c
Adeel <adeeliftikhar67@gmail.com>
2019-06-16 18:38:39 +0500

    Deleted

--- ChaiePaani/app/src/main/java/com/example/muhammadashfaq/eatit/Interface/ItemClickListner.java
+++
@@ -1,7 +0,0 @@
-package com.example.muhammadashfaq.eatit.Interface;
-
-import android.view.View;
-
-public interface ItemClickListner {
-    void onClick(View view,int position,boolean isLongClick);
-}
